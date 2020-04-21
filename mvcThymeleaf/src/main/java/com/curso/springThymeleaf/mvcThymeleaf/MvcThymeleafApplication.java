d7a06b5491aa5025872e44889b672f0ca61d2130
Nathan Aguiar <nathanaguiarbs@gmail.com>
2020-04-21 17:58:10 -0300

    primeiro commit

---
+++ mvcThymeleaf/src/main/java/com/curso/springThymeleaf/mvcThymeleaf/MvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springThymeleaf.mvcThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ mvcThymeleaf/src/test/java/com/curso/springThymeleaf/mvcThymeleaf/MvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springThymeleaf.mvcThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
